15e5fe9438c25881da7f21ecceb56493f69edb7a
Simay Mutlu <simay706@gmail.com>
2025-12-25 01:48:04 +0300

    fixed DatabaseAdapter.java

--- src/main/java/com/group18/greengrocer/dao/DatabaseAdapter.java
+++ src/main/java/com/group18/greengrocer/dao/DatabaseAdapter.java
@@ -3,8 +3,6 @@ package com.group18.greengrocer.dao;
 import com.group18.greengrocer.util.Constants;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
