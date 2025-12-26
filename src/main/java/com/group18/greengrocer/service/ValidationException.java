79fa3f513ff6285f2596223e72bfacb3eb5229b5
silasmsk <zsimsek@stu.khas.edu.tr>
2025-12-26 14:34:00 +0300

    ValidationException added

--- src/main/java/com/group18/greengrocer/service/ValidationException.java
+++ src/main/java/com/group18/greengrocer/service/ValidationException.java
@@ -4,7 +4,14 @@ package com.group18.greengrocer.service;
  * Exception thrown when a business rule or validation check fails.
  */
 public class ValidationException extends RuntimeException {
+
+    
     public ValidationException(String message) {
         super(message);
+    }
+
+    
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
